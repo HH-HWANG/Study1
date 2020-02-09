49c1454d6376b5ee2fea7f8aa2eb122592fbf3e6
HHH <HHH@DESKTOP-GSJMP2V>
2020-02-09 17:59:23 +0900

    origin

---
+++ src/One.java
@@ -0,0 +1,15 @@
+public class One{
+	/**
+	 * 
+	 * @param args
+	 */
+	public static void main(String args[]) {
+//		주석문 컨트롤 + / 
+		System.out.println("lotto");
+		System.out.println("test");
+		System.out.println("1");
+		System.out.println("2");
+		System.out.println("3");
+		
+	}
+}
---
+++ src/One2.java
@@ -0,0 +1,6 @@
+public class One2{
+	public static void main(String args[]) {
+		System.out.println("lotto");
+		
+	}
+}
